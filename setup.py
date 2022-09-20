c83812125d27b2edd5afe67241713e642b3da641
SoshunNaito <soshun1005hamburg@gmail.com>
2022-09-20 23:57:08 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     description='Ising Machine-Assisted Quantum Compiler',
     author='Soshun Naito',
     author_email='soshun1005hamburg@gmail.com',
-    packages=find_packages(),
-    package_data={'isaaq': ['data/**/*.*']},
+    data_files={'': ['data/**/*.*']},
 )
