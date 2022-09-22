4fdd39db32b154d8aa9560e18ec91dda6ec71df1
SoshunNaito <soshun1005hamburg@gmail.com>
2022-09-23 07:51:43 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup, find_packages
-# from glob import glob
-# import os
-
-# filenames = list(glob('isaaq/data/**/*.*', recursive=True))
-# files = dict()
-# for filename in filenames:
-#     dirname = os.path.dirname(filename)
-#     if(dirname not in files): files[dirname] = [filename]
-#     else: files[dirname].append(filename)
 
 setup(
     name='isaaq',
@@ -17,5 +8,4 @@ setup(
     author_email='soshun1005hamburg@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    # data_files=sorted(list(files.items())),
 )
