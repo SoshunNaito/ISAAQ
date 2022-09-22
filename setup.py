040f5047119a2ba98daf6d07e5f4cc90c4d295c2
SoshunNaito <soshun1005hamburg@gmail.com>
2022-09-23 07:20:23 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
-from glob import glob
-import os
+# from glob import glob
+# import os
 
-filenames = list(glob('isaaq/data/**/*.*', recursive=True))
-files = dict()
-for filename in filenames:
-    dirname = os.path.dirname(filename)[6:]
-    if(dirname not in files): files[dirname] = [filename]
-    else: files[dirname].append(filename)
+# filenames = list(glob('isaaq/data/**/*.*', recursive=True))
+# files = dict()
+# for filename in filenames:
+#     dirname = os.path.dirname(filename)[6:]
+#     if(dirname not in files): files[dirname] = [filename]
+#     else: files[dirname].append(filename)
 
 setup(
     name='isaaq',
@@ -17,5 +17,5 @@ setup(
     author_email='soshun1005hamburg@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    data_files=sorted(list(files.items())),
+    # data_files=sorted(list(files.items())),
 )
