cbfd955ae62536c99293a53b8c1666dd8f8a7dba
SoshunNaito <soshun1005hamburg@gmail.com>
2022-10-31 14:39:46 +0900

    removed dummy QUBO cost

--- isaaq/Clustering/ClusteredMappingSolver.py
+++ isaaq/Clustering/ClusteredMappingSolver.py
@@ -6,7 +6,8 @@ from isaaq.Clustering import ClusteringResult
 def SolveClusteredMapping(
     inputCircuit: QuantumCircuit, layerSize: int,
     clusteringResult: ClusteringResult, 
-    QAPScheduler: BaseQAPScheduler
+    QAPScheduler: BaseQAPScheduler,
+    useLocalSearch: bool = False
 ) -> QubitMapping:
 
     mappingProblem: QubitMappingProblem = None
@@ -14,6 +15,7 @@ def SolveClusteredMapping(
     for i in range(clusteringResult.numClusterDevices):
         if(i == 0):
             mappingProblem = GenerateMappingProblem(inputCircuit, clusteringResult.clusterDevices[0], maxLayerSize = layerSize)
+            mappingResult = QAPScheduler.solve(mappingProblem)
         else:
             mappingProblem = QubitMappingProblem(
                 clusteringResult.clusterDevices[i], mappingProblem.layers,
@@ -25,5 +27,22 @@ def SolveClusteredMapping(
                     ] for layer in mappingResult.layers
                 ]
             )
-        mappingResult = QAPScheduler.solve(mappingProblem)
+            mappingResult = QAPScheduler.solve(mappingProblem)
+
+            if(useLocalSearch):
+                graph = clusteringResult.clusterDevices[i].graph
+                mappingProblem = QubitMappingProblem(
+                    clusteringResult.clusterDevices[i], mappingProblem.layers,
+                    [
+                        [
+                            # [
+                            #     n for n in clusteringResult.clusterMappings[i][layer.virtualToPhysical[v]]
+                            # ]
+                            list(graph.neighbours[layer.virtualToPhysical[v]] | {layer.virtualToPhysical[v]})
+                            for v in range(inputCircuit.numQubits)
+                        ] for layer in mappingResult.layers
+                    ]
+                )
+                mappingResult = QAPScheduler.solve(mappingProblem)
+
     return mappingResult
