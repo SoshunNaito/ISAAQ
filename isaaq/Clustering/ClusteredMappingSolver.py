62e9b1c00890c8d2510ff68621a875b095e92877
SoshunNaito <soshun1005hamburg@gmail.com>
2024-11-18 11:29:50 +0900

    useLocalSearchをlocalSearchCountに変更した

--- isaaq/Clustering/ClusteredMappingSolver.py
+++ isaaq/Clustering/ClusteredMappingSolver.py
@@ -1,51 +1,65 @@
+from isaaq.Clustering import ClusteringResult
 from isaaq.Common import QuantumCircuit, QubitMapping, QubitMappingProblem
 from isaaq.Problem import GenerateMappingProblem
 from isaaq.Scheduler import BaseQAPScheduler
-from isaaq.Clustering import ClusteringResult
+
 
 def SolveClusteredMapping(
     inputCircuit: QuantumCircuit,
-    clusteringResult: ClusteringResult, 
+    clusteringResult: ClusteringResult,
     QAPScheduler: BaseQAPScheduler,
     maxLayerSize: int = -1,
     minLayerCount: int = 1,
     simplifyCircuit: bool = False,
-    useLocalSearch: bool = False
+    localSearchCount: int = 0,
 ) -> QubitMapping:
 
     mappingProblem: QubitMappingProblem = None
     mappingResult: QubitMapping = None
     for i in range(clusteringResult.numClusterDevices):
-        if(i == 0):
+        if i == 0:
             mappingProblem = GenerateMappingProblem(
                 inputCircuit,
                 clusteringResult.clusterDevices[0],
-                maxLayerSize, minLayerCount, simplifyCircuit
+                maxLayerSize,
+                minLayerCount,
+                simplifyCircuit,
             )
             mappingResult = QAPScheduler.solve(mappingProblem)
         else:
             mappingProblem = QubitMappingProblem(
-                clusteringResult.clusterDevices[i], mappingProblem.layers,
+                clusteringResult.clusterDevices[i],
+                mappingProblem.layers,
                 [
                     [
                         [
-                            n for n in clusteringResult.clusterMappings[i][layer.virtualToPhysical[v]]
-                        ] for v in range(inputCircuit.numQubits)
-                    ] for layer in mappingResult.layers
-                ]
+                            n
+                            for n in clusteringResult.clusterMappings[i][
+                                layer.virtualToPhysical[v]
+                            ]
+                        ]
+                        for v in range(inputCircuit.numQubits)
+                    ]
+                    for layer in mappingResult.layers
+                ],
             )
             mappingResult = QAPScheduler.solve(mappingProblem)
 
-        if(useLocalSearch):
+        for _count in range(localSearchCount):
             graph = clusteringResult.clusterDevices[i].graph
             mappingProblem = QubitMappingProblem(
-                clusteringResult.clusterDevices[i], mappingProblem.layers,
+                clusteringResult.clusterDevices[i],
+                mappingProblem.layers,
                 [
                     [
-                        list(graph.neighbours[layer.virtualToPhysical[v]] | {layer.virtualToPhysical[v]})
+                        list(
+                            graph.neighbours[layer.virtualToPhysical[v]]
+                            | {layer.virtualToPhysical[v]}
+                        )
                         for v in range(inputCircuit.numQubits)
-                    ] for layer in mappingResult.layers
-                ]
+                    ]
+                    for layer in mappingResult.layers
+                ],
             )
             mappingResult = QAPScheduler.solve(mappingProblem)
 
